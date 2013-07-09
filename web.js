c80af6913786ec1327b61c03606a342b6f6e8b4d
alan.stewart <stewaala@gmail.com>
2013-07-09 20:38:23 +0000

    removed path name

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var b = new Buffer('I am a string',"utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  b=fs.readFileSync('~/index.html');
+  b=fs.readFileSync('index.html');
   response.send(b.toString('utf-8'));
 });
 
